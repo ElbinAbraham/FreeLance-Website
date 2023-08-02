17ba42c8b9f8a8e9c60d890ae50e4172d4226aa9
ElbinAbraham <105164867+ElbinAbraham@users.noreply.github.com>
2023-08-02 20:27:55 +0530

    Create Home.js

---
+++ Components/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import SplideSlider from '../Slider.js';
+import About1 from '../About1';
+import CardItems from '../card2';
+import About2 from '../About2';
+import About3 from '../About3';
+import Footer from '../footer';
+import images from '../image.js';
+import { useNavigate } from "react-router-dom";
+import details from '../details.js';
+
+function Home() {
+  const navigate = useNavigate();
+  function handleCardClick(card) {
+    navigate("/category", { state: { category: card.category, subCategory: card.subCategory } });
+  }
+
+  return (
+    <>
+      <SplideSlider images={images} />
+      <About1 />
+      <CardItems card={details} onCardClick={handleCardClick} />
+      <About2 />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
