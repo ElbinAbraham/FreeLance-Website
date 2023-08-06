fc94dbde2243c36605be46ec225d1075617daac6
Elbin <elbinthomas@student.sfit.ac.in>
2023-08-06 19:14:19 +0530

    Latest Updates

--- src/Components/card2.js
+++ src/Components/card2.js
@@ -24,7 +24,7 @@ const CardItems = ({
               <CardMedia
                 component="img"
                 image={card.src}
-                alt={card.alt}
+                alt={card.name}
                 height="140"
                 sx={{ width: "100%", height: "80%", objectFit: "fill" }}
                 onClick={() => onCardClick(card)}
