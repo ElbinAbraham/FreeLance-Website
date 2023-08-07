10c338c2e6c918e2461f34d153e5cb4ab43fef14
Elbin <elbinthomas@student.sfit.ac.in>
2023-08-07 19:32:04 +0530

    Latest Updates

--- src/Components/slick-desc.js
+++ src/Components/slick-desc.js
@@ -1,28 +1,29 @@
 import React from "react";
-import '../Components/slick-des.css';
+import "../Components/slick-des.css";
 
 const SlickDesc = ({ desc }) => {
-    console.log("mine"+desc);
-    const points = Object.keys(desc).filter(key => key.startsWith("point")).map(key => desc[key]);
-    
-    return (
-        <>
-            {desc.title && (
-                <div className="slick-desc">
-                    <div className="slick-desc-title">
-                        <h1> {desc.title} 
-                        {console.log("me"+desc.title)}
-                        </h1>
-                    </div>
-                    <div className="slick-desc-text">
-                        {points.map((point, index) => (
-                            <p key={index}> {point} </p>
-                        ))}
-                    </div>
-                </div>
-            )}
-        </>
-    );
-}
+  const points = Object.keys(desc)
+    .filter((key) => key.startsWith("point"))
+    .map((key) => desc[key]);
+
+  return (
+    <>
+      {desc.title && (
+        <div className="slick-desc">
+          <div className="slick-desc-title">
+            <h1>
+              <strong>{desc.title}</strong>
+            </h1>
+          </div>
+          <div className="slick-desc-text">
+            {points.map((point, index) => (
+              <p key={index} dangerouslySetInnerHTML={{ __html: point }} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default SlickDesc;
